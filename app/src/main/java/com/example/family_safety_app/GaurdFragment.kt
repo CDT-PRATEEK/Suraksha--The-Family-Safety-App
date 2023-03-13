84e2dd3589078e7e6c3503a7065dbc9583fe4f76
Prateek <prateeksinha510@gmail.com>
2023-03-14 00:20:53 +0530

    Basic UI and Android Recycler View Implemented.

---
+++ app/src/main/java/com/example/family_safety_app/GaurdFragment.kt
@@ -0,0 +1,29 @@
+package com.example.family_safety_app
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class GaurdFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_gaurd, container, false)
+    }
+
+    companion object {
+
+        fun newInstance() = GaurdFragment()
+
+            }
+}
---
+++ app/src/main/java/com/example/family_safety_app/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.family_safety_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomBar = findViewById<BottomNavigationView>(R.id.bottom_bar)
+
+        bottomBar.setOnItemSelectedListener {
+
+            if(it.itemId == R.id.nav_gaurd){
+                inflateFragment(GaurdFragment.newInstance())
+
+            }
+            else if(it.itemId == R.id.nav_home){
+                inflateFragment(HomeFragment.newInstance())
+
+            }
+            else if(it.itemId == R.id.nav_dashboard){
+                inflateFragment(DashboardFragment.newInstance())
+            }
+            else if(it.itemId == R.id.nav_profile){
+                inflateFragment(ProfileFragment.newInstance())
+            }
+
+            true
+
+
+        }
+
+        bottomBar.selectedItemId = R.id.nav_home
+
+
+        }
+
+    private fun inflateFragment(newInstance : Fragment) {
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,newInstance)
+        transaction.commit()
+
+    }
+
+}
+
+
+
+
+
